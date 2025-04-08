718b14df87d8891b5203c3a8c061c85e7ee83ecd
AiLin <103014845+wanglin214@users.noreply.github.com>
2025-04-08 21:13:00 +0800

    Update plot_inv_err_depth_density.py

--- DL_interface_inversion/utils/plot_inv_err_depth_density.py
+++ DL_interface_inversion/utils/plot_inv_err_depth_density.py
@@ -1,26 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 假设数据保存在一个 CSV 文件中，以空格分隔
+# Load data from a space-separated file
 errpath = r'D:\Project\DL_interface_inversion\utils\total_err_1e100_scale.txt'
-# 如果数据是以其他方式分隔的，可以修改 sep 参数
+# If data is separated differently, modify the sep parameter
 df = pd.read_csv(errpath, sep=' ')
 
-# 选择需要的列，这里选择前10列
+# Select specific column (7th column, index 6)
 selected_columns = df.iloc[:, 6]
 
-# 将选定列的值展平成一个 Series
+# Flatten the selected column data into a Series
 flat_data = selected_columns.values
 
-# 绘制直方图
-plt.hist(flat_data, bins=20, edgecolor='black',density=True)
+# Create histogram
+plt.hist(flat_data, bins=20, edgecolor='black', density=True)
+
+# Set x-axis label
+# Multiple label options are commented out - uncomment the one needed
 # plt.xlabel('RMSE of depth inversion/m')
 # plt.xlabel('ABSE of density inversion/(g/cm³)')
 # plt.xlabel('RMSE of reconstructed gravity anomaly/mGal')
 # plt.xlabel('Morphology Similarity')
 plt.xlabel('Relative RMSE of depth inversion')
 
+# Set y-axis label
 plt.ylabel('Frequency')
+
+# Title is commented out
 # plt.title('Histogram')
+
+# X-axis limits are commented out
 # plt.xlim(-1000, 1000)
+
+# Display the plot
 plt.show()
+ 

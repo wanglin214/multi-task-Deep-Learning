65b389800aeaf9cf7ae6bac392b1c7e090ce581f
AiLin <103014845+wanglin214@users.noreply.github.com>
2025-04-08 20:44:42 +0800

    Update dataload.py

--- DL_interface_inversion/utils/dataload.py
+++ DL_interface_inversion/utils/dataload.py
@@ -1,29 +1,16 @@
 import os
-
 import torch
 # import numpy as np
 # import torch.nn.functional as fun
-from torch.utils.data import Dataset  # 构造数据集，支持索引，总长度
+from torch.utils.data import Dataset  # For constructing datasets with indexing and length support
 # from torch.utils.data import DataLoader
 
 from utils.readGrd import readGrdbynp
 
-"""
-深度学习训练框架/步骤：
-   1. prepare dataset
-      tools: Dataset and DataLoader
-   2. Design model using Class
-      inherit from nn.moudle
-   3. Construct loss and optimizer
-      using Pytorch API
-   4. Training cycle
-   forward, backward, update
-"""
-
 
 class GravityDataset(Dataset):
 
-    def __init__(self, root: str, train: str):  # root指定根目录,resize自定义数据大小
+    def __init__(self, root: str, train: str):  # root specifies root directory, resize customizes data size
         super(GravityDataset, self).__init__()
 
         self.train = train
@@ -34,44 +21,54 @@ class GravityDataset(Dataset):
         elif self.train == 'validation':
             self.root = os.path.join(root, 'Validation')
 
-        assert os.path.join(self.root), f"path '{self.root}' does not exists."  # 判断文件路径是否存在
+        assert os.path.join(self.root), f"path '{self.root}' does not exists."  # Check if file path exists
 
+        # Get all gravity anomaly files
         dg_names = [i for i in os.listdir(os.path.join(self.root, "dg")) if i.endswith(".grd")]
-        self.dg_list = [os.path.join(self.root, "dg", i) for i in dg_names]  # 获取dg文件夹下的全部grd文件
+        self.dg_list = [os.path.join(self.root, "dg", i) for i in dg_names]  # Get all grd files in the dg folder
         self.interface_list = [str(i) for i in range(len(self.dg_list))]
         # self.density = [i for i in range(len(self.dg_list))]
-        for i in range(len(self.dg_list)):  # 让异常与对应的模型label数据匹配
-            censtr = self.dg_list[i][-17:-12]  # 取出文件中间编号部分
+        
+        # Match anomalies with corresponding model label data
+        for i in range(len(self.dg_list)):
+            censtr = self.dg_list[i][-17:-12]  # Extract the middle part of the file number
             # print(censtr)
             # os.system('pause')
             self.interface_list[i] = os.path.join(self.root, 'model', 'SedofBasin_' + censtr + '.grd')
 
-        # check files
-        for i in self.dg_list:  # 注意for i in self.dg_list或者dg_names中i为字符串类型而非整形
+        # Check if files exist
+        for i in self.dg_list:  # Note that i in self.dg_list or dg_names is string type not integer
             if os.path.exists(i) is False:
                 raise FileExistsError(f"file {i} doesn't exissts.")
 
-    def __getitem__(self, index):  # 按照文件列表当前索引下标对应的sample与label
+    def __getitem__(self, index):  # Get sample and label corresponding to current index in file list
         dg = readGrdbynp(self.dg_list[index])
         label_interface = readGrdbynp(self.interface_list[index])
-        # 获取文件名中密度编号
+        
+        # Get density number from filename
         numdens = self.dg_list[index][-6:-4]
         label_dens = 0.1 + (int(numdens) - 1) * (0.7 - 0.1) / 40.0
         label_dens = torch.tensor(label_dens)
         # print(numdens, label_dens)
         # os.system('pause')
-        # dg = (dg - np.mean(dg)) / np.std(dg)   #对异常进行标准化操作,使得数据满足均值为0，方差为1
-        # dg = (dg - np.min(dg)) / (np.max(dg)-np.min(dg))  # 最大最小归一化[0,1]
-        # numpy数组传入torch
+        
+        # Data normalization options (commented out)
+        # dg = (dg - np.mean(dg)) / np.std(dg)   # Standardize anomaly to mean=0, std=1
+        # dg = (dg - np.min(dg)) / (np.max(dg)-np.min(dg))  # Min-max normalization to [0,1]
+        
+        # Convert numpy arrays to torch tensors
         label_interface = torch.from_numpy(label_interface).unsqueeze(0)
         dg = torch.from_numpy(dg).unsqueeze(0)
         # print(mod_label.shape,dg.shape)
-        dg = dg * (-1.0)
-        mod_label = label_interface  # 设置输入输出为单精度float16 类型Tensor
-        # # 对数据重采样重构成网络目标输入大小200*200, 200*200，二维数据interpolate函数输入需为[b,c,h,w]
+        dg = dg * (-1.0)  # Invert gravity anomaly sign
+        mod_label = label_interface  # Set input/output as float16 type Tensor
+        
+        # Resampling options (commented out)
+        # # Resample data to network target input size 200*200, 200*200, 2D data interpolate function input needs [b,c,h,w]
         # dg_sample = fun.interpolate(dg, size=[64, 64], mode='bilinear', align_corners=True)
         # lab_interface_sample = fun.interpolate(mod_label, size=[64, 64], mode='bilinear', align_corners=True)
-        #  将batch通道数维度去掉
+        
+        # Remove batch dimension
         dg_sample = dg
         label_interface_sample = mod_label
         label_dens = label_dens.unsqueeze(0)
@@ -83,7 +80,7 @@ class GravityDataset(Dataset):
         return len(self.dg_list)
 
 
-# # # 测试数据集读取是否成功
+# # # Test if dataset loading is successful
 if __name__ == '__main__':
     myroot = r"D:\Project\DL_interface_inversion\data"
     mydata = GravityDataset(myroot, train='train')
